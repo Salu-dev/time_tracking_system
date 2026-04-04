7ad4d10c5021db778bd5fd035672389ad965d916
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 <salubaiju2803@gmail.com>
2026-04-04 18:06:23 +0000

    feat:sales visit doctype and validations

--- time_tracking_system/time_tracking_system/doctype/sales_visit/sales_visit.js
+++ time_tracking_system/time_tracking_system/doctype/sales_visit/sales_visit.js
@@ -1,8 +1,63 @@
 // Copyright (c) 2026, salu and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Sales Visit", {
-// 	refresh(frm) {
+frappe.ui.form.on("Sales Visit", {
+	refresh(frm) {
+        // add button to mark updations
+        frm.trigger("add_mark_updates_button");
 
-// 	},
-// });
+	},
+    add_mark_updates_button(frm) {
+
+        // add check in button
+        if (frm.doc.status=="Scheduled") {
+            frm.add_custom_button("Check In", () => {
+                frm.doc.status = "In Progress";
+                get_location().then(location => {
+                    frm.doc.check_in_latitude = location.latitude;
+                    frm.doc.check_in_longitude = location.longitude;
+                    frm.dirty();
+                    frm.save();
+                });
+            });
+            frm.add_custom_button("Cancel", () => {
+                frm.doc.status = "Cancelled";
+                frm.dirty();
+                frm.save();
+            });
+       
+        }
+        else if (frm.doc.status=="In Progress") {
+            frm.add_custom_button("Check Out", () => {
+                frm.doc.status = "Completed";
+                get_location().then(location => {
+                    console.log(location);
+                    frm.doc.check_out_latitude = location.latitude;
+                    frm.doc.check_out_longitude = location.longitude;
+                    frm.dirty();
+                    frm.save();
+                });
+            });
+        } 
+    },
+    
+    
+        
+});
+
+function get_location() {
+    return new Promise((resolve, reject) => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position) => {
+               let latitude = position.coords.latitude;
+               let longitude = position.coords.longitude;
+               resolve({ latitude, longitude });
+            }, (error) => {
+                reject(error);
+            });
+        } else {
+            reject(new Error('Geolocation is not supported by this browser.'));
+        }
+    });
+}
+
--- time_tracking_system/time_tracking_system/doctype/sales_visit/sales_visit.py
+++ time_tracking_system/time_tracking_system/doctype/sales_visit/sales_visit.py
@@ -1,9 +1,63 @@
 # Copyright (c) 2026, salu and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
-
+from time_tracking_system.utils import get_customer_location
 
 class SalesVisit(Document):
-	pass
+	def before_insert(self):
+		self.date = frappe.utils.nowdate()
+		# self.sales_person = frappe.session.user
+		if self.customer:
+			customer_location = get_customer_location(self.customer)
+			if customer_location:
+				self.latitude = customer_location.custom_lttitude 
+				self.longitude = customer_location.custom_longitude
+		
+	def validate(self):
+		# check if travel start time is set before check in
+		if self.check_in and not self.travel_start_time:
+			frappe.throw("Travel start time is required when check in is set")
+		if self.check_in and self.travel_start_time:
+			if self.travel_start_time > self.check_in:
+				frappe.throw("Travel start time cannot be after check in time")
+		if self.check_out and self.check_in:
+			if self.check_out < self.check_in:
+				frappe.throw("Check out time cannot be before check in time")
+		# # check duplicate schedule 
+		# if self.customer and self.scheduled_date:
+		# 	visit = frappe.db.get_value("Sales Visit", {"customer": self.customer, 
+		# 	"scheduled_date": self.scheduled_date,"sales_person": self.sales_person,
+		# 	"status": ["not in", ["Cancelled", "Completed"]]}, "name")
+		# 	if visit:
+		# 		frappe.throw("Sales visit already exists for this customer and scheduled date with status not cancelled or completed")
+
+
+	def before_save(self):
+		old_doc = self.get_doc_before_save()
+		if old_doc and old_doc.status != self.status:
+			if self.status == "In Progress":
+				self.check_in = frappe.utils.now()
+				if self.travel_start_time:
+					self.travel_time = frappe.utils.time_diff_in_hours(self.check_in, self.travel_start_time)
+			elif self.status == "Completed":
+				self.check_out = frappe.utils.now()
+				self.time_spend = frappe.utils.time_diff_in_hours(self.check_out, self.check_in)
+		if old_doc and (old_doc.customer != self.customer or old_doc.address != self.address):
+			customer_location = get_customer_location(self.customer)
+			if customer_location:
+				self.latitude = customer_location.custom_lattitude
+				self.longitude = customer_location.custom_longitude
+			
+@frappe.whitelist()
+def check_and_update_pending_visits():
+    """Check and update pending sales visits"""
+    try:
+        pending_visits = frappe.get_all("Sales Visit", {"status":["not in", ["Cancelled", "Completed"]], "scheduled_date": ["<", frappe.utils.nowdate()]},pluck="name")
+        for visit in pending_visits:
+            frappe.db.set_value("Sales Visit", visit, "status", "Pending")
+    except Exception as e:
+        frappe.log_error(f"Error in check_and_update_pending_visits: {str(e)}")
+        frappe.throw(f"Error updating pending visits: {str(e)}")
+		
